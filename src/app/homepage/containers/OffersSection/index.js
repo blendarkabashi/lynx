2a6f06ac7b1cd2bd2ac3a9cb5c334838f0f7edb2
drinfe <39890985+drinfejzullahu@users.noreply.github.com>
2024-10-07 17:43:10 +0200

    added offers

---
+++ src/app/homepage/containers/OffersSection/index.js
@@ -0,0 +1,92 @@
+import Section from "@/components/Section";
+import React from "react";
+
+const offers = [
+  {
+    image: "/assets/offers/4-day-gjergjevica.png",
+    title: (
+      <>
+        7 day <br />
+        CAT-SKIING
+        <br />
+        GJERAVICA
+      </>
+    ),
+    subtitle: "2800€",
+  },
+  {
+    image: "/assets/offers/4-day-hut.png",
+    title: (
+      <>
+        4 day <br />
+        CAT-SKIING
+        <br />
+        GJERAVICA
+      </>
+    ),
+    subtitle: "1500€",
+  },
+  {
+    image: "/assets/offers/7-day-gjergjevica.png",
+    title: (
+      <>
+        7 day <br />
+        HUT TO HUT
+        <br />
+        SKI TOURING
+      </>
+    ),
+    subtitle: "2000€",
+  },
+  {
+    image: "/assets/offers/7-day-hut.png",
+    title: (
+      <>
+        4 day <br />
+        HUT TO HUT
+        <br />
+        SKI TOURING
+      </>
+    ),
+    subtitle: "1200€",
+  },
+];
+
+const OffersSection = () => {
+  return (
+    <div className="bg-[#070B36] pt-[67px] pb-[72px]">
+      <Section>
+        <div className="flex space-x-8 overflow-y-">
+          {offers.map((offer, index) => (
+            <div
+              key={index}
+              className="relative bg-cover bg-center
+                w-[174px] h-[200px]        // Default for phone
+                tablet:w-[194px] tablet:h-[222px]   // Tablet size
+                desktop:w-[289px] desktop:h-[330px] // Desktop size"
+              style={{
+                backgroundImage: `url(${offer.image})`,
+              }}
+            >
+              {/* Text on top of the image */}
+              <div className="absolute top-6 left-6 text-white text-lg font-semibold">
+                {offer.title}
+              </div>
+
+              <div className="absolute bottom-6 left-6 text-white text-lg font-semibold">
+                {offer.subtitle}
+              </div>
+
+              {/* Icon at the bottom-right */}
+              <div className="absolute bottom-2 right-3">
+                <img src="/assets/arrow-right.svg" alt="Arrow Icon" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </Section>
+    </div>
+  );
+};
+
+export default OffersSection;
--- src/app/homepage/page.js
+++ src/app/homepage/page.js
@@ -2,12 +2,14 @@ import Header from "@/components/Header";
 import HeroSection from "./containers/HeroSection";
 import PartnersSection from "./containers/PartnersSection";
 import Footer from "./containers/FooterSection";
+import OffersSection from "./containers/OffersSection";
 
 const Homepage = () => {
   return (
     <div className="h-[200vh] bg-black">
       <Header />
       <HeroSection />
+      <OffersSection />
       <PartnersSection />
       <Footer />
     </div>
