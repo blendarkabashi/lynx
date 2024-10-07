f38020002f701774c1e7190648092cf631e0b6e1
blendar-opgny <blendar@opgny.com>
2024-10-07 19:10:03 +0200

    responsive menu work

---
+++ src/components/Icons/LynxIcon/index.js
@@ -0,0 +1,14 @@
+const LynxIcon = () => {
+  return (
+    <svg width="20" height="35" viewBox="0 0 20 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M17.5632 5.13337L19.8398 0.972656H18.5705L16.3184 5.13337H17.5632Z" fill="#F2BB05" />
+      <path
+        d="M15.2549 7.09751L10.3574 16.1431L5.50938 7.09751H4.16536L9.78784 17.2397L9.79128 34.706H11.0135V17.2397L16.4886 7.09751H15.2549Z"
+        fill="white"
+      />
+      <path d="M4.45642 5.13337L2.22663 0.972656H0.828125L3.09493 5.13337H4.45642Z" fill="#F2BB05" />
+    </svg>
+  );
+};
+
+export default LynxIcon;
