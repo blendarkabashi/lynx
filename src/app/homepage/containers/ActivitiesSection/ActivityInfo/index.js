467dde470434541d909cab7f74004d031a050802
blendar-opgny <blendar@opgny.com>
2024-10-08 11:37:43 +0200

    fix padding on top for ski touring

--- src/app/homepage/containers/ActivitiesSection/ActivityInfo/index.js
+++ src/app/homepage/containers/ActivitiesSection/ActivityInfo/index.js
@@ -5,7 +5,7 @@ const ActivityInfo = ({ firstImage, secondImage, title, richtext, color, mobileI
     <div className={`${color} pt-[35px] pb-[50px] tablet:py-[45px] desktop:py-[70px]`}>
       <Section className="flex flex-col tablet:flex-row">
         <img className="hidden tablet:block tablet:w-[260px] desktop:w-[395px] -m-[25px]" src={firstImage} />
-        <div className="tablet:px-[30px] mt-[34px]">
+        <div className="tablet:px-[30px] tablet:mt-[34px]">
           <h1 className="mb-3 text-[25px] leading-[45px] tablet:text-[30px] desktop:leading-[55px] font-black uppercase text-[#070B36] desktop:text-[45px]">
             {title}
           </h1>
